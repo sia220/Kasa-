76c625abf5e3dc9191a834f16cf89894689e1b35
Sadia <Esadia@hotmail.fr>
2024-06-02 13:36:29 +0100

    suppression ligne de code inutile et importation fichier de données

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import kasaList from "./datas/kasaList"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img  className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
