47fe099d2440fb96a3eb7213c06a9620b19b7be1
Sadia <Esadia@hotmail.fr>
2024-06-03 13:01:39 +0100

    mise en place des routes vers les 4 pages du site

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/Home';
+import Home from './pages/Home';
+import Housing from './pages/Housing'
+import Error from './pages/Error'
+import Apropos from './pages/A-propos'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
       <Router>
           <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/logement" element={<Housing />} />
+              <Route path="/a-propos" element={<Apropos />} />
+              <Route path="*"  element={<Error />} />
           </Routes>
                 </Router>
   </React.StrictMode>,
