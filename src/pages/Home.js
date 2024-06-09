8c737215e0d549fb4420cd71d6becf48a603bc8e
Sadia <Esadia@hotmail.fr>
2024-06-09 13:31:38 +0100

    utilisation du contexte PageContext pour afficher le texte de banner et utilisation de la props image pour afficher l'image de banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,28 @@
-import "../styles/home.scss"
-import Header from "../components/Header";
-import Footer from "../components/Footer"
-import Gallery from "../components/Gallery"
-import Banner from "../components/Banner"
+import '../styles/home.scss';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Gallery from '../components/Gallery';
+import Banner from '../components/Banner';
+import BannerImg from '../assets/home.png';
+import { PageContext } from '../utils/context/index';
+import { useContext, useEffect } from 'react';
 
 function Home() {
-  return (
-    <div className="App">
-      <Header/>
-      <main className="main">
-        <Banner/>
-        <Gallery/>
-      </main>
-      <Footer/>
-    </div>
-  );
+   const { page, setPage } = useContext(PageContext);
+   useEffect(() => {
+      setPage(page === 'apropos' ? 'home' : 'home');
+   }, []);
+
+   return (
+      <div className="App">
+         <Header />
+         <main className="main">
+            <Banner image={BannerImg} />
+            <Gallery />
+         </main>
+         <Footer />
+      </div>
+   );
 }
 
 export default Home;
