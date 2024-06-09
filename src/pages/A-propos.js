bb0c1673a8c54f2b475ae8e04f028d58376cdcbc
Sadia <Esadia@hotmail.fr>
2024-06-09 13:32:31 +0100

    appel des composants Header, Banner et Footer

--- src/pages/A-propos.js
+++ src/pages/A-propos.js
@@ -1,7 +1,25 @@
-import kasaList from "../datas/kasaList"
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Banner from '../components/Banner';
+import BannerImg from '../assets/apropos.png';
+import { PageContext } from '../utils/context/index';
+import { useContext, useEffect } from 'react';
 
 function Apropos() {
-  
+   const { setPage, page } = useContext(PageContext);
+   useEffect(() => {
+      setPage(page === 'home' ? 'apropos' : 'apropos');
+   }, []);
+
+   return (
+      <div className="App">
+         <Header />
+         <main className="main">
+            <Banner image={BannerImg} />
+         </main>
+         <Footer />
+      </div>
+   );
 }
 
 export default Apropos;
