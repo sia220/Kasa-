3c50ba71b00d7c8cbe5ed2994c4415020231b003
Sadia <Esadia@hotmail.fr>
2024-06-10 13:13:57 +0100

    codage du composant error + style

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,25 @@
-import kasaList from "../datas/kasaList"
+import { Link } from 'react-router-dom';
+import QuatreCentQuatre from '../assets/404.svg';
+import Header from '../components/Header';
+import '../styles/error.scss';
 
 function Error() {
-  
+   return (
+      <div>
+         <Header />
+         <main className="main--error">
+            <div className="main--div">
+               <img src={QuatreCentQuatre} alt="404"></img>
+               <p className="main--div--p">
+                  Oups! La page que vous demandez n'existe pas.
+               </p>
+            </div>
+            <Link className="main--link" to="/">
+               Retourner sur la page d’accueil
+            </Link>
+         </main>
+      </div>
+   );
 }
 
 export default Error;
