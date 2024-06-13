d647c48d1e7e5ad56f97e14ed33c5ebace98a658
Sadia <Esadia@hotmail.fr>
2024-06-13 11:19:33 +0100

    ajout des composants Header, Footer et SlideShow utilisation du contexte pour maj la page consultée

--- src/pages/Housing.js
+++ src/pages/Housing.js
@@ -1,10 +1,25 @@
-import kasaList from "../datas/kasaList"
-import Footer from '../components/Footer'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import SlideShow from '../components/SlideShow';
+import { PageContext } from '../utils/context/index';
+import { useContext, useEffect } from 'react';
+import '../styles/housing.scss';
 
 function Housing() {
-  return (
-    <Footer/>
-  )
+   const { setPage } = useContext(PageContext);
+   useEffect(() => {
+      setPage('housing');
+   }, []);
+
+   return (
+      <div>
+         <Header />
+         <main className="main--housing">
+            <SlideShow />
+         </main>
+         <Footer />
+      </div>
+   );
 }
 
 export default Housing;
