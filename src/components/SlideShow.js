da800920a8ff7ba0bafc72da8a0dd30026a1c65f
Sadia <Esadia@hotmail.fr>
2024-07-01 13:25:47 +0100

    - ajustement pour que les flèches du slideshow tournent à l'infini - on retire les fleches et le compteur pour les logements avec une seule image

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -18,40 +18,66 @@ function SlideShow() {
    return (
       <div className="slideShow">
          <img className="slideShow--background" src={img} alt="logement"></img>
-         <img
-            className="slideShow--arrowLeft"
-            src={ArrowLeft}
-            alt="précedent"
-            onClick={() => {
-               logementConsulte.pictures.map((element, index) =>
-                  element === img[0]
-                     ? index > 0
-                        ? setImg([logementConsulte.pictures[index - 1]])
-                        : null
-                     : null
-               );
-            }}></img>
-         <img
-            className="slideShow--arrowRight"
-            src={ArrowRight}
-            alt="suivant"
-            onClick={() => {
-               logementConsulte.pictures.map((element, index) =>
-                  element === img[0]
-                     ? index !== NbPictures - 1
-                        ? setImg([logementConsulte.pictures[index + 1]])
-                        : null
-                     : null
-               );
-            }}></img>
-         {logementConsulte.pictures.map(
-            (element, index) =>
-               element === img[0] && (
-                  <p className="slideShow--compteur">
-                     {index + 1} / {NbPictures}
-                  </p>
-               )
-         )}
+         {
+            // Si le logement a plusieurs photos à afficher
+            NbPictures !== 1 && (
+               <img
+                  className="slideShow--arrowLeft"
+                  src={ArrowLeft}
+                  alt="précedent"
+                  onClick={() => {
+                     // On parcourt les photos du logement présentes dans le fichier JSON
+                     logementConsulte.pictures.map((element, index) =>
+                        element === img[0]
+                           ? // Si l'image affichée n'est pas la première du caroussel
+                             index > 0
+                              ? // Alors on affiche l'image précédente
+                                setImg([logementConsulte.pictures[index - 1]])
+                              : // Sinon, on affiche la dernière image du caroussel
+                                setImg([
+                                   logementConsulte.pictures[NbPictures - 1],
+                                ])
+                           : null
+                     );
+                  }}></img>
+            )
+         }
+
+         {
+            // Si le logement a plusieurs photos à afficher
+            NbPictures !== 1 && (
+               <img
+                  className="slideShow--arrowRight"
+                  src={ArrowRight}
+                  alt="suivant"
+                  onClick={() => {
+                     // On parcourt les photos du logement présentes dans le fichier JSON
+                     logementConsulte.pictures.map((element, index) =>
+                        element === img[0]
+                           ? // Si l'image affichée n'est pas la dernière du caroussel
+                             index !== NbPictures - 1
+                              ? // Alors on affiche l'image suivante
+                                setImg([logementConsulte.pictures[index + 1]])
+                              : // Sinon, on affiche la première image du caroussel
+                                setImg([logementConsulte.pictures[0]])
+                           : null
+                     );
+                  }}></img>
+            )
+         }
+         {
+            // On parcourt les photos du logement présentes dans le fichier JSON
+            logementConsulte.pictures.map((element, index) =>
+               element === img[0] ? (
+                  // Si le logement a plusieurs photos à afficher
+                  NbPictures !== 1 ? (
+                     <p className="slideShow--compteur">
+                        {index + 1} / {NbPictures}
+                     </p>
+                  ) : null
+               ) : null
+            )
+         }
       </div>
    );
 }
