966d7c9cb9915927b1490b9ff7c07ed3a5530c44
Sadia <Esadia@hotmail.fr>
2024-07-05 20:26:03 +0100

    responsive bannière tablette et mobile

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,10 +5,17 @@ import { useContext } from 'react';
 function Banner({ image }) {
    const { page } = useContext(PageContext);
    return (
-      <section className="banner">
+      <section
+         className={
+            page === 'home' ? 'banner banner__home' : ' banner banner__apropos'
+         }>
          <img className="banner--img" src={image} alt="Nature" />
          {page === 'home' && (
-            <h1 className="banner--titre"> Chez vous, partout et ailleurs</h1>
+            <h1 className="banner--titre">
+               {' '}
+               Chez vous, <br className="banner--titre--br"></br> partout et
+               ailleurs
+            </h1>
          )}
          <div className="banner--background"></div>
       </section>
