696e13682f226256cf0a9b5ef4411d1aa4aa859a
Sadia <Esadia@hotmail.fr>
2024-07-05 20:31:32 +0100

    modification du composant collapse pour qu'il puisse être utiliser sur la page de logement

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -3,12 +3,26 @@ import ArrowTop from '../assets/arrow-top.svg';
 import ArrowBottom from '../assets/arrow-bottom.svg';
 import '../styles/collapse.scss';
 import PropTypes from 'prop-types';
+import { PageContext } from '../utils/context/index';
+import { useContext } from 'react';
 
 function Collapse({ titre, description }) {
+   const { page } = useContext(PageContext);
    const [isClose, setIsClose] = useState(true);
+
    return (
-      <div className="collapse">
-         <div className="collapse--haut">
+      <div
+         className={
+            page === 'housing'
+               ? 'collapse collapse__housing'
+               : 'collapse collapse__apropos'
+         }>
+         <div
+            className={
+               page === 'housing'
+                  ? 'collapse--haut collapse--haut__housing'
+                  : 'collapse--haut collapse--haut__apropos'
+            }>
             <h2 className="collapse--haut--titre">{titre}</h2>
             <img
                className="collapse--haut--img"
@@ -17,8 +31,25 @@ function Collapse({ titre, description }) {
                src={isClose === true ? ArrowBottom : ArrowTop}></img>
          </div>
          {isClose === false && (
-            <div className="collapse--bas">
-               <p className="collapse--bas--desc">{description}</p>
+            <div
+               className={
+                  page === 'housing'
+                     ? 'collapse--bas collapse--bas__housing'
+                     : 'collapse--bas collapse--bas__apropos'
+               }>
+               {
+                  // Si la props description est une chaîne de caractères, alors on utilise une balise p
+                  typeof description === 'string' ? (
+                     <p className="collapse--bas--desc">{description}</p>
+                  ) : (
+                     // Si la props description est un tableau, alors on utilise une balise ul et chaque élement est placé dans une balise li.
+                     <ul className="collapse--bas--desc">
+                        {description.map((element, index) => (
+                           <li key={`equipements--${index}`}>{element}</li>
+                        ))}
+                     </ul>
+                  )
+               }
             </div>
          )}
       </div>
@@ -27,7 +58,6 @@ function Collapse({ titre, description }) {
 
 Collapse.propTypes = {
    titre: PropTypes.string,
-   description: PropTypes.string,
 };
 
 export default Collapse;
