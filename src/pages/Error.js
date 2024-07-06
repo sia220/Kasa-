b553750a08cacd6214486bbfb0381b9ae61972cd
Sadia <Esadia@hotmail.fr>
2024-07-06 12:46:07 +0100

    responsive tablette et mobile de la page d'erreur

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -15,9 +15,13 @@ function Error() {
          <Header />
          <main className="main--error">
             <div className="main--div">
-               <img src={QuatreCentQuatre} alt="404"></img>
-               <p className="main--div--p">
-                  Oups! La page que vous demandez n'existe pas.
+               <img
+                  className="main--div--img"
+                  src={QuatreCentQuatre}
+                  alt="404"></img>
+               <p className="main--div--texte">
+                  Oups! La page que <br className="main--div--texte--br"></br>{' '}
+                  vous demandez n'existe pas.
                </p>
             </div>
             <Link className="main--link" to="/">
