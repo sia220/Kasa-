680464918572d82188c8cea3669b4977194b0507
Sadia <Esadia@hotmail.fr>
2024-07-06 12:16:08 +0100

    - ajout section qui englobe les collapses - responsive des collapses de la page A propos

--- src/pages/A-propos.js
+++ src/pages/A-propos.js
@@ -18,22 +18,24 @@ function Apropos() {
          <Header />
          <main className="main--apropos">
             <Banner image={BannerImg} />
-            <Collapse
-               titre="Fiabilité"
-               description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
-            />
-            <Collapse
-               titre="Respect"
-               description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-            />
-            <Collapse
-               titre="Service"
-               description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-            />
-            <Collapse
-               titre="Sécurité"
-               description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-            />
+            <section className="collapses">
+               <Collapse
+                  titre="Fiabilité"
+                  description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+               />
+               <Collapse
+                  titre="Respect"
+                  description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+               />
+               <Collapse
+                  titre="Service"
+                  description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+               />
+               <Collapse
+                  titre="Sécurité"
+                  description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+               />
+            </section>
          </main>
          <Footer />
       </div>
